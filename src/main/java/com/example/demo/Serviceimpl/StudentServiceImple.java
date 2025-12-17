0629b6d7631d66b714a8dadf738adca38dbb5ad8
bala-amypo <bala@amypo.in>
2025-12-17 10:41:04 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
@@ -1,4 +1,4 @@
-package com.example.demo Serviceimpl;
+package com.example.demo.ServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,12 +8,13 @@ import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 
 @Service
-public class StudentServiceImple implements StudentService  {
-    @Autowired
-    StudentRepository studentRepository;
+public class StudentServiceImple implements StudentService {
 
-    public Student saveStudent(Student student){
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
         return studentRepository.save(student);
     }
-
 }
