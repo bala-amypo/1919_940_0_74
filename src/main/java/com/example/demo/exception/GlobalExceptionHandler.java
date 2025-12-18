20fd6b3fc87238167a260af938aadba967f9eec1
bala-amypo <bala@amypo.in>
2025-12-18 08:20:28 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -3,14 +3,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class GlobalExceptionhandler{
 
-    @ExceptionHandler(ResourceNotFoundException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)//runtime exception
         public ResponseEntity<String>handleNotFound(ResourceNotFoundException ex){
             return new ResponsiveEntity<>.(ex.getMessage(),HttpStatus.NOT_FOUND);
         } 
     
-    @ExceptionHandler()
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>>
 }
